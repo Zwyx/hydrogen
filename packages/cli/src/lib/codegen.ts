df673cac83279febf4fa5ac62d7f6fe7245e4702
Michelle Chen <michelle.chen@shopify.com>
2023-12-14 16:11:37 -0500

    ✨ add type generation for customer account api in app/graphql/customer folder

--- packages/cli/src/lib/codegen.ts
+++ packages/cli/src/lib/codegen.ts
@@ -194,6 +194,20 @@ async function generateDefaultConfig(
   const defaultGlob = '*!(*.d).{ts,tsx,js,jsx}'; // No d.ts files
   const appDirRelative = relativePath(rootDirectory, appDirectory);
 
+  const caapiSchema = getSchema('customer');
+  const caapiProject = findGqlProject(caapiSchema, gqlConfig);
+
+  var customerAPIConfig;
+  if (caapiProject?.documents) {
+    customerAPIConfig = {
+      ['customerapi.generated.d.ts']: {
+        preset,
+        schema: caapiSchema,
+        documents: caapiProject?.documents,
+      },
+    };
+  }
+
   return {
     filepath: 'virtual:codegen',
     config: {
@@ -228,6 +242,7 @@ async function generateDefaultConfig(
             },
           }),
         },
+        ...customerAPIConfig,
       },
     },
   };
